4a4cb9ee8d936a99bd098e28700e5083559e780a
EliasOliveiraFerreira <grumec951@gmail.com>
2022-01-26 15:20:06 -0300

    vai saber

--- dist/server.js
+++ dist/server.js
@@ -5,4 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT;
-app_1.default.listen(PORT, () => console.log(`Pai ta on familia o_O on PORT ${PORT}`));
+app_1.default.listen(PORT || 3001, () => console.log(`Pai ta on familia o_O on PORT ${PORT}`));
