3ae7315508cd47a8a88fe3d8d3679330ff5797db
EliasOliveiraFerreira <grumec951@gmail.com>
2022-01-26 15:19:08 -0300

    modeify ports on server

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@ import app from './app'
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log(`Pai ta on familia o_O on PORT ${PORT}`))
+app.listen(PORT || 3001, () =>
+  console.log(`Pai ta on familia o_O on PORT ${PORT}`)
+)
