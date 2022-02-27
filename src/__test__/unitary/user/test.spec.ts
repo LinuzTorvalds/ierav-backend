d777605265086973837450a844e2f0fce185a0d4
ieravrio <ieraveng@gmail.com>
2022-02-27 12:59:37 -0300

    Nada o_O

--- src/__test__/unitary/user/test.spec.ts
+++ src/__test__/unitary/user/test.spec.ts
@@ -20,38 +20,24 @@ describe('nada º~º', () => {
     var sunday, saturday
 
     if (today.getDay() === 0) {
-      sunday = moment(today).format('DD-MM')
+      sunday = moment(today)
 
-      saturday = moment(today).add(6, 'days').format('DD-MM')
+      saturday = moment(today).add(6, 'days')
     } else {
       const days = 7 - today.getDay()
 
-      sunday = moment(today).add(days, 'days').toDate()
+      sunday = moment(today).add(days, 'days')
 
-      saturday = moment(sunday).add(6, 'days').format('DD-MM')
-
-      sunday = moment(sunday).format('DD-MM')
+      saturday = moment(sunday).add(6, 'days')
     }
 
     let listBirthdays = []
 
     for (let user of birthdays) {
-      const birthday = moment(user.birthday).add(1, 'days').format('DD-MM-YYYY')
+      const birthday = moment(user.birthday).add(1, 'days')
 
-      const start: string = sunday
-
-      const end: string = saturday
-
-      if (
-        birthday.substring(3, 5) == start.substring(3, 5) ||
-        birthday.substring(3, 5) == end.substring(3, 5)
-      ) {
-        if (
-          birthday.substring(0, 2) >= start.substring(0, 2) &&
-          birthday.substring(0, 2) <= end.substring(0, 2)
-        ) {
-          listBirthdays.push({ ...user })
-        }
+      if (birthday >= sunday || birthday <= saturday) {
+        listBirthdays.push({ ...user })
       }
     }
 
@@ -63,37 +49,24 @@ describe('nada º~º', () => {
     let listWeddingAnniversary = []
 
     for (let user of weddings_Aninversary) {
-      const weddingAnniversary = moment(user.birthday)
-        .add(1, 'days')
-        .format('DD-MM-YYYY')
+      const weddingAnniversary = moment(user.birthday).add(1, 'days')
 
-      const start: string = sunday
-
-      const end: string = saturday
-      if (
-        weddingAnniversary.substring(3, 5) == start.substring(3, 5) ||
-        weddingAnniversary.substring(3, 5) == end.substring(3, 5)
-      ) {
-        if (
-          weddingAnniversary.substring(0, 2) >= start.substring(0, 2) &&
-          weddingAnniversary.substring(0, 2) <= end.substring(0, 2)
-        ) {
-          let year = moment(today)
-            .subtract(weddingAnniversary.substring(6, 10), 'year')
-            .format('YYYY')
-          let data: wedding
-          if (year.substring(0, 2) === '00')
-            data = await prisma.wedding.findFirst({
-              where: { year: year.substring(2, 4) },
-            })
-          else
-            data = await prisma.wedding.findFirst({
-              where: { year: year.substring(1, 4) },
-            })
-          const wedding = data.description
-          year = data.year
-          listWeddingAnniversary.push({ ...user, wedding, year })
-        }
+      if (weddingAnniversary >= sunday || weddingAnniversary <= saturday) {
+        let year = moment(today)
+          .subtract(moment(weddingAnniversary).format('YYYY'), 'year')
+          .format('YYYY')
+        let data: wedding
+        if (year.substring(0, 2) === '00')
+          data = await prisma.wedding.findFirst({
+            where: { year: year.substring(2, 4) },
+          })
+        else
+          data = await prisma.wedding.findFirst({
+            where: { year: year.substring(1, 4) },
+          })
+        const wedding = data.description
+        year = data.year
+        listWeddingAnniversary.push({ ...user, wedding, year })
       }
     }
 
@@ -102,5 +75,8 @@ describe('nada º~º', () => {
     result.listBirthdays = listBirthdays
 
     result.listWeddingAnniversary = listWeddingAnniversary
+
+    console.log(result.listBirthdays)
+    console.log(result.listWeddingAnniversary)
   })
 })
